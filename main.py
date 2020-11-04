8bfad7a5d1d96aab079b171a781766f3d65fecc2
Alex Verrico <contact@alexverrico.com>
2020-11-04 21:31:17 +1100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import os
 import datetime
 import flask
 from dotenv import load_dotenv
+import json
 
 # Initialize stuff
 
@@ -79,11 +80,23 @@ def insert_templates(page):
     template_list = [{'file': 'head.html', 'string': 'head'},
                      {'file': 'nav.html', 'string': 'nav'},
                      {'file': 'footer.html', 'string': 'footer'},
-                     {'file': 'core.js', 'string': 'corejs'}]
+                     {'file': 'core.html', 'string': 'corejs'}]
     for template in template_list:
         with open(f'html/templates/{template["file"]}', 'r') as f:
             page = page.replace(f'%%%{template["string"]}%%%', f.read())
     return page
+
+
+def auth(_uid, _auth, _type='system'):
+    _connection = sqlite3.connect(dbPath)
+    _cursor = _connection.cursor()
+    _auth_cmd = "SELECT password FROM auth WHERE id = ?"
+    # temp = cursor.execute("SELECT password FROM systems_auth WHERE id = %s" % str(_id)).fetchone()
+    _temp = _cursor.execute(_auth_cmd, (str(_uid),)).fetchone()
+    if str(_auth) == str(_temp[0]):
+        return True
+    else:
+        return False
 
 
 # Flask stuff
@@ -101,7 +114,64 @@ def entry_by_id():
 
 
 # Then we declare the api paths:
-# @app.route()
+@app.route('/api/v1/seek/by_id', methods=['GET'])
+def seek_by_id():
+    if 'uid' not in flask.request.args or 'auth' not in flask.request.args or 'id' not in flask.request.args:
+        return '', 400
+    else:
+        _uid = str(flask.request.args['uid'])
+        _auth = str(flask.request.args['auth'])
+        _id = str(flask.request.args['id'])
+    if auth(_uid, _auth) is False:
+        return '', 400
+    select_statement = "SELECT * FROM entries WHERE id = ?"
+    con = sqlite3.connect(dbPath)
+    cur = con.cursor()
+    cur.execute(select_statement, (_id,))
+    x = cur.fetchone()
+    con.close()
+    if x is None:
+        return flask.jsonify({'name': 'Invalid ID', 'id': _id}), 200
+    out = dict()
+    out['id'] = x[0]
+    out['name'] = x[1]
+    out['origin'] = x[2]
+    out['retrieval_date'] = x[3]
+    out['location'] = x[4]
+    out['path'] = x[5]
+    out['tags'] = x[6]
+    return flask.jsonify(out), 200
+
+
+@app.route('/api/v1/update/by_id', methods=['POST'])
+def update_by_id():
+    if 'uid' not in flask.request.values or 'auth' not in flask.request.values or 'id' not in flask.request.values or 'data' not in flask.request.values:
+        return '', 400
+    else:
+        _uid = str(flask.request.values['uid'])
+        _auth = str(flask.request.values['auth'])
+        _id = str(flask.request.values['id'])
+        _data = json.loads(flask.request.values['data'])
+    if auth(_uid, _auth) is False:
+        return '', 400
+    con = sqlite3.connect(dbPath)
+    cur = con.cursor()
+    select_statement = "SELECT * FROM entries WHERE id = ?"
+    cur.execute(select_statement, [_uid])
+    db_data = dict()
+    if cur.fetchone() is None:
+        db_data['action'] = 'create'
+    else:
+        db_data['action'] = 'update'
+    db_data['scope'] = 'row'
+    db_data['dbPath'] = dbPath
+    db_data['table'] = 'entries'
+    db_data['column'] = 'id'
+    db_data['columnValue'] = _id
+    dbOperations.put(db_data)
+    pass
+
+
 # Run functions
 
 
