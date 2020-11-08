f73b72eb9de326ca4ea1349430112023f2eed07c
Alex Verrico <contact@alexverrico.com>
2020-11-08 19:10:18 +1100

    Update main.py to generate static html on start instead of re-generating for each request

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import os
 import flask
 from dotenv import load_dotenv
 import json
+import shutil
+
 
 # Initialize stuff
 
@@ -21,52 +23,9 @@ dbPath = "".join((Basedir, "main.sqlite"))
 dbOperations = Queue(maxsize=0)
 
 app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
 
 # Declare Functions
-
-
-# def add_entry(e_name=None, e_origin=None, e_retrieval_date=None, e_location=None, e_path=None, e_tags=None, _id=None):
-#     if not e_name:
-#         e_name = "unnamed"
-#     if not e_origin:
-#         e_origin = "unknown"
-#     if not e_retrieval_date:
-#         e_retrieval_date = "unknown"
-#     if not e_location:
-#         e_location = "unknown"
-#     if not e_path:
-#         e_path = "unknown"
-#     if not e_tags:
-#         e_tags = "unknown"
-#
-#     if _id:
-#         print(_id)
-#         pass
-#     else:
-#         e_name = str(e_name)
-#         e_origin = str(e_origin)
-#         e_retrieval_date = str(e_retrieval_date)
-#         e_location = str(e_location)
-#         e_path = str(e_path)
-#         e_tags = str(e_tags).split(" ")
-#         print(e_tags)
-#         con = sqlite3.connect(dbPath)
-#         cur = con.cursor()
-#         # values = id,name,origin,retrieval_date,location,path,tags
-#         cur.execute("SELECT _contents FROM misc_stuff WHERE id = '000001'")
-#         _id = int(cur.fetchone()[0]) + 1
-#         _id = str(_id)
-#         _id = _id.zfill(6)
-#         print(_id)
-#         id_update_statement = "UPDATE misc_stuff SET _contents = ? WHERE id = '000001'"
-#         cur.execute(id_update_statement, (_id,))
-#         insert_statement = 'INSERT INTO entries values(?, ?, ?, ?, ?, ?, ?)'
-#         _e_tags = e_tags[0]
-#         for i in range(1, len(e_tags)):
-#             _e_tags = "".join((_e_tags, " ", e_tags[i]))
-#         cur.execute(insert_statement, (str(_id), e_name, e_origin, e_retrieval_date, e_location, e_path, _e_tags,))
-#         con.commit()
-#         return True
 
 
 def run_queue():
@@ -119,6 +78,41 @@ def insert_templates(page):
     return page
 
 
+def recursive_listdir(path):
+    out = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for file in filenames:
+            if file.endswith('.html'):
+                out.append("/".join([dirpath, file]))
+    return out
+
+
+def build_static_pages():
+    pages_dir = 'html/pages'
+    built_dir = 'built'
+    shutil.rmtree(built_dir)
+    os.mkdir(built_dir)
+    filelist = recursive_listdir(pages_dir)
+    out = []
+    for file in filelist:
+        file = file[len(pages_dir):]
+        while file.startswith('/'):
+            file = file[1:]
+        while file.startswith('\\'):
+            file = file[1:]
+        out.append(file)
+    for file in out:
+        page = insert_templates(''.join((pages_dir, '/', file)))
+        path = file.rsplit('/', 1)
+        path = ''.join((built_dir, '/', path[0]))
+        if not path.endswith('.html'):
+            if not os.path.exists(path):
+                os.makedirs(path)
+        with open(''.join((built_dir, '/', file)), 'w') as f:
+            f.write(page)
+    return True
+
+
 def auth(_uid, _auth, _type='system'):
     _connection = sqlite3.connect(dbPath)
     _cursor = _connection.cursor()
@@ -134,26 +128,7 @@ def auth(_uid, _auth, _type='system'):
 # First we declare the normal paths:
 @app.route('/', methods=['GET'])
 def home():
-    page = insert_templates('html/pages/home.html')
-    return page, 200
-
-
-@app.route('/entry_by_id/', methods=['GET'])
-def entry_by_id():
-    page = insert_templates('html/pages/entry_by_id.html')
-    return page, 200
-
-
-@app.route('/find_by_name/', methods=['GET'])
-def find_by_name():
-    page = insert_templates('html/pages/find_by_name.html')
-    return page, 200
-
-
-@app.route('/find_by_tag/', methods=['GET'])
-def find_by_tag():
-    page = insert_templates('html/pages/find_by_tag.html')
-    return page, 200
+    return flask.send_from_directory('built', 'index.html')
 
 
 # Then we declare the api paths:
@@ -285,9 +260,30 @@ def update_by_id():
     return '', 200
 
 
+# And finally we define a catch-all route to serve the built pages
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if '..' in str(path):
+        return '', 404
+    try:
+        x = path.split('.')[1]
+        x = True
+    except IndexError:
+        path = ''.join((path, '/'))
+        x = False
+    if path.endswith('/'):
+        path = ''.join((path, 'index.html'))
+    return flask.send_from_directory('built', path)
+
+
 # Run functions
 
 t = Timer(0, run_queue)
 t.start()  # Start dbOperations Queue
+
+print("Building pages....")
+build_static_pages()
+print("Built pages.")
 
 app.run('0.0.0.0', 5515)
